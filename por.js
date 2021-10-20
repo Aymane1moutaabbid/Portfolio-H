00c0ad68c8aa21c748d8966444511c1b81bf8fd3
Aymane Moutabbid <moutaabidayman34@gmail.com>
2021-10-20 16:34:07 +0100

    complate responsive  design

---
+++ por.js
@@ -0,0 +1,7 @@
+function show() {
+    if(document.getElementById('mobil-menu').style.display=='none') {
+      document.getElementById('mobil-menu').style.display='block';
+    }else{
+      document.getElementById('mobil-menu').style.display='none'
+    }
+ }
