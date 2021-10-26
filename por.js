9989da2cda9e1b304528240ac971b48bee32f117
Aymane Moutabbid <moutaabidayman34@gmail.com>
2021-10-26 15:50:29 +0100

    modification menu mobile

--- por.js
+++ por.js
@@ -1,7 +1,16 @@
 function show() {
-    if(document.getElementById('mobil-menu').style.display=='none') {
-      document.getElementById('mobil-menu').style.display='block';
-    }else{
-      document.getElementById('mobil-menu').style.display='none'
-    }
- }
+  if (document.getElementById("mobil-menu").style.display == "none") {
+    document.getElementById("mobil-menu").style.display = "block";
+  }
+  document.body.style.overflow = "hidden";
+}
+function exit() {
+  if (document.getElementById("mobil-menu").style.display == "block") {
+    document.getElementById("mobil-menu").style.display = "none";
+  }
+  document.body.style.overflow = "auto";
+}
+
+document
+  .querySelectorAll(".mobil-menu a, .mobil-menu .exit")
+  .forEach((item) => item.addEventListener("click", exit));
